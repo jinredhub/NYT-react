0c191087707559028faf2c9d4ecdf3648f6a8558
jinredhub <jin@nieblo.com>
2017-12-09 19:43:47 -0800

    btn float right

--- client/src/components/Result/DeleteBtn.js
+++ client/src/components/Result/DeleteBtn.js
@@ -3,7 +3,7 @@ import React from "react";
 export const DeleteBtn = props => 
 
 	<button 
-		className="btn btn-danger"
+		className="btn btn-danger float-right"
 		onClick={()=>props.deleteArticle(props.snippet)}>{props.children}
 	</button>
 
--- client/src/components/Result/SaveBtn.js
+++ client/src/components/Result/SaveBtn.js
@@ -3,7 +3,7 @@ import React from "react";
 export const SaveBtn = props => 
 
 	<button 
-		className="btn btn-success"
+		className="btn btn-success float-right"
 		onClick={()=>props.saveArticle(props.snippet, props.url)}>{props.children}
 	</button>
 
