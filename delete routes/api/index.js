b556bd9524ed337c46a279450c204077f4176fd3
jinredhub <jin@nieblo.com>
2017-12-09 21:00:38 -0800

    testing heroku env

--- delete routes/api/index.js
+++
@@ -1,7 +0,0 @@
-// const router = require("express").Router();
-// const ArticleRoutes = require("./Articles");
-
-// // Article routes
-// router.use("/books", ArticleRoutes);
-
-// module.exports = router;
